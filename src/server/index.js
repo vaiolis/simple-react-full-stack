52d648a54da1cba919bfd0506163ed18baa6d5b7
vaiolis <harrisonllin@gmail.com>
2022-08-05 11:33:46 -0700

    Foobar code

--- src/server/index.js
+++ src/server/index.js
@@ -5,5 +5,6 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/foobar', (req, res) => res.send({ foo: 'bar' }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
